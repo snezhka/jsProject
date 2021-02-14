d59d886a45cd6706daca59e223322cc7dbb21171
Valeriy Kokorev <kokorev.valery@gmail.com>
2021-02-14 13:05:35 +0700

    Добавил генерацию тестовых данных.

--- js/main.js
+++ js/main.js
@@ -34,8 +34,116 @@ const getRandomFloat = (min, max, digits) => {
   return parseFloat(result.toFixed(digits));
 };
 
-// eslint-disable-next-line no-console
-console.log(getRandomInt(0, 100));
+/**
+ * Getting a random element from array.
+ * @param {Array.<Object>} array An array for getting an element
+ * @returns {Object}
+ */
+const getRandomArrayElement = (array) =>  {
+  if (array.length === 0) {
+    return null;
+  }
+  return array[getRandomInt(0, array.length)];
+};
+
+
+/**
+ * Getting a random count random unique elements from array.
+ * @param {Array.<Object>} array An array for getting an elements
+ * @returns {Array.<Object>}
+ */
+const getRandomArrayElements = (array) => {
+  const result = [];
+
+  for (let i = 0; i <= getRandomInt(0, array.length); i++) {
+    result.push(getRandomArrayElement(array))
+  }
+
+  return result.filter((value, index, self) => {
+    return self.indexOf(value) === index;
+  });
+}
+
+/**
+ * List of housing types
+ * @type {string[]}
+ */
+const housingTypes = ['palace', 'flat', 'house', 'bungalow'];
+
+/**
+ * List times of checkin and checkout
+ * @type {string[]}
+ */
+const checkTimes = ['12:00', '13:00', '14:00'];
+
+/**
+ * List of housing futures
+ * @type {string[]}
+ */
+const housingFutures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+/**
+ * List of housing photos
+ * @type {string[]}
+ */
+const housingPhotos = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+
+/**
+ * ctor random author data
+ * @constructor
+ */
+const TestAuthor = function () {
+  const AVATARS_PATH = 'img/avatars/';
+  const avatarFileName = `user0${getRandomInt(1, 9)}.png`;
+  this.avatar = AVATARS_PATH + avatarFileName;
+};
+
+/**
+ * ctor random location data
+ * @constructor
+ */
+const TestLocation = function () {
+  this.x = getRandomFloat(35.65, 35.7, 5);
+  this.y = getRandomFloat(139.7, 139.8, 5);
+};
+
+/**
+ * ctor random offer data
+ * @param location
+ * @constructor
+ */
+const TestOffer = function (location) {
+  location = location || new TestLocation();
+
+  this.title = 'Тестовое объявление';
+  this.address = `${location.x}, ${location.y}`;
+  this.price = getRandomFloat(1, 10000, 2);
+  this.type = getRandomArrayElement(housingTypes);
+  this.rooms = getRandomInt(1, 11);
+  this.guests = getRandomInt(1, 11);
+  this.checkin = getRandomArrayElement(checkTimes);
+  this.checkout = getRandomArrayElement(checkTimes);
+  this.features = getRandomArrayElements(housingFutures);
+  this.description = 'Описание тестового объявления';
+  this.photos = getRandomArrayElements(housingPhotos);
+};
+
+/**
+ * ctor random housing data
+ * @constructor
+ */
+const TestHousing = function () {
+  const location = new TestLocation();
+
+  this.author =  new TestAuthor();
+  this.offer = new TestOffer(location);
+  this.location = location;
+};
+
+const housings = new Array(10).fill(null).map(() => new TestHousing());
 
 // eslint-disable-next-line no-console
-console.log(getRandomFloat(0, 100, 5));
+console.log(housings)
