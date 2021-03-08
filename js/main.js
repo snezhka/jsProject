1d49f8396737a2875d6afa986099556afba920af
Valeriy Kokorev <kokorev.valery@gmail.com>
2021-03-08 22:52:18 +0700

    Удалил тестовую отрисовку объявления

--- js/main.js
+++ js/main.js
@@ -1,8 +1,2 @@
 'use strict';
 
-import {TestHousing} from './test-data.js';
-import {Card} from './card.js';
-
-const mapCanvas = document.querySelector('#map-canvas');
-const card = new Card(new TestHousing());
-mapCanvas.append(card.render());
